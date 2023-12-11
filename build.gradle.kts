45f165e2debe227dfd006ed6ceb7561c012aa239
YangSiJun528 <yangsijun5528@gmail.com>
2023-12-11 15:23:08 +0900

    gradle 버전 업그레이드 8.2 -> 8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    annotationProcessor("dev.yangsijun.field_specific_builder:specific-builder-processor:1.0")
-    compileOnly("dev.yangsijun.field_specific_builder:specific-builder-annotations:1.0")
+//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+    //annotationProcessor("dev.yangsijun.field_specific_builder:specific-builder-processor:1.0")
+    //compileOnly("dev.yangsijun.field_specific_builder:specific-builder-annotations:1.0")
 }
 
 tasks.test {
